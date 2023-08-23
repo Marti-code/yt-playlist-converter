9d5451f38fbadfccaae0d9b540bfb984799ed154
Marti-code <marti.huza@gmail.com>
2023-08-23 21:22:58 +0200

    download playlist base

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask import Blueprint, render_template, request, jsonify, redirect, send_file, url_for
 from pytube import Playlist, YouTube
+
+import os
+import zipfile
+
 
 
 views = Blueprint(__name__, "views")
@@ -43,8 +47,30 @@ def go_to_json():
 def download():
     playlist_url = request.form['playlist_url']
     playlist = Playlist(playlist_url)
+    download_folder = 'downloads/'
+
+    # Create a folder for downloaded videos
+    os.makedirs(download_folder, exist_ok=True)
 
     for video in playlist.videos:
-        video.streams.get_highest_resolution().download('downloads/')  # Change the download directory as needed
+        video.streams.get_highest_resolution().download(download_folder)
 
-    return "Download completed!"
+    # Create a zip file containing the downloaded videos
+    zipfile_name = 'downloaded_videos.zip'
+    with zipfile.ZipFile(zipfile_name, 'w') as zipf:
+        for root, _, files in os.walk(download_folder):
+            for file in files:
+                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), download_folder))
+
+    # Send the zip file to the user for download
+    return send_file(zipfile_name, as_attachment=True)
+
+# def down():
+#     try:
+#         playlist_url = request.form['playlist_url']
+#         ytObject = YouTube(playlist_url)
+#         ytVideo = ytObject.streams.get_highest_resolution()
+#         ytVideo.download()
+#     except:
+#         print('YouTube link invalid')
+#     print("Success!")
